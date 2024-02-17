adc5e99e00eff9397375958adc2694936f84c787
Md Musfikur Rahman <107528767+Md-Musfikur-Rahman@users.noreply.github.com>
2024-02-17 19:47:45 +0600

    tags rearrange

--- src/sections/home/index.ts
+++ src/sections/home/index.ts
@@ -1,11 +1,7 @@
 export { default as Pricing } from './Pricing';
 export { default as HomeHero } from './HomeHero';
 export { default as HomeMinimal } from './HomeMinimal';
-export { default as HomeDarkMode } from './HomeDarkMode';
 export { default as HomeLookingFor } from './HomeLookingFor';
-export { default as HomeForDesigner } from './HomeForDesigner';
 export { default as HomeColorPresets } from './HomeColorPresets';
 export { default as HomePricingPlans } from './HomePricingPlans';
 export { default as HomeAdvertisement } from './HomeAdvertisement';
-export { default as HomeCleanInterfaces } from './HomeCleanInterfaces';
-export { default as HomeHugePackElements } from './HomeHugePackElements';
